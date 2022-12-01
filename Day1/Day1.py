2abc9643a0d213f00be80bb41bed3a8600e4d2df
Keiran <ifvracer@gmail.com>
2022-12-01 15:47:51 +0000

    Day 1 Code & File

---
+++ Day1/Day1.py
@@ -0,0 +1,15 @@
+def get_calorie_sums():
+    with open('/Users/oakleyk/Documents/Coding/Python/AdventOfCode2022/Day1/Day1Input.txt', 'r') as file:
+        return [sum(map(int, x.split('\n'))) for x in file.read().strip().split('\n\n')]
+
+
+def get_top_calories():
+    return max(get_calorie_sums())
+
+
+def get_top_three_calories_sum():
+    return sum(sorted(get_calorie_sums())[-3:])
+
+
+print(get_top_calories())
+print(get_top_three_calories_sum())
